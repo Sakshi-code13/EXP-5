8c3398073667c730f3c58049aebd4bf9bd9a6e43
Sakshi-code13 <sakshi132728@gmail.com>
2024-03-01 00:04:20 +0530

    Committing in branch1

--- Sum.c
+++ Sum.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    int number1, number2, sum;
+    int a, b, sum;
     
     printf("Enter two integers: ");
-    scanf("%d %d", &number1, &number2);
+    scanf("%d %d", &a, &b);
     
     // calculate the sum
-    sum = number1 + number2;
+    sum = a + b;
     
-    printf("%d + %d = %d", number1, number2, sum);
+    printf("%d + %d = %d", a, b, sum);
     
     return 0;
 }
